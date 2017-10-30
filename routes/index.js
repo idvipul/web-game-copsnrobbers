b948713d112dedca9e9320376a199fb0dcfbc348
Umang Mathur <umangmathur10051992@gmail.com>
2017-10-30 10:49:09 -0700

    deleted all files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Term Project CSC 667/867' });
-});
-
-module.exports = router;
