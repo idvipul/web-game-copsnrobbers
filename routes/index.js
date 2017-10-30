306777a49398d4efd7b2fc3f17f4642d9a038643
Umang Mathur <umangmathur10051992@gmail.com>
2017-10-29 19:36:31 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Term Project CSC 667/867' });
+});
+
+module.exports = router;
