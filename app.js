7f5e85255cd4e53c42ab11ca5f71af0a7268ff60
Umang Mathur <umangmathur10051992@gmail.com>
2017-10-30 15:39:38 -0700

    added test route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var tests = require('./routes/tests');
 
 if(process.env.NODE_ENV === 'development') {
 require("dotenv").config();
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/tests', tests);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/tests.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const db = require('../db/index.js');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	db.any(`INSERT INTO test_table ("testString") VALUES ('Hello at ${Date.now()}')`)
+.then( _ => db.any(`SELECT * FROM test_table`) )
+.then( results => res.json( results ) )
+.catch( error => {
+console.log( error )
+res.json({ error })
+});
+  //res.send('This is the test response');
+});
+
+module.exports = router;
