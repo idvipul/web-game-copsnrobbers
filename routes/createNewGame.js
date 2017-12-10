9f644ffe1b97cc75f0a0919236e600603432300a
Girish Tiwale <girishmt4@gmail.com>
2017-12-09 18:27:58 -0800

    playerRole updates when game is created

--- routes/createNewGame.js
+++ routes/createNewGame.js
@@ -2,19 +2,25 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db/index.js');
 var crypto = require('crypto');
-
+const url=require('url');
 function randomValueHex(len) {
     return crypto.randomBytes(Math.ceil(len / 2)).toString('hex').slice(0, len);
 }
 
 var value = randomValueHex(6);
 
-router.get('/', function (req, res, next) {
-
-//    res.json(req.body.playerRole);
-
+router.post('/', function (req, res, next) {
+    var role=req.body.playerRole;
+    
     const gameId = value;
-    res.redirect('/game/' + gameId);
-});
+    res.redirect(url.format({
+        pathname:"/game/"+gameId,
+        query:{
+            "playerRole":role,
+            "user":req.user
+        }
+    }));
 
+    //res.redirect('/game/' + gameId,);
+});
 module.exports = router;
--- routes/game.js
+++ routes/game.js
@@ -13,22 +13,56 @@ router.get('/:gameId', function (req, res, next) {
 //    var Xposition = ;
 //    var Yposition = ;
 //    var playerRole= ;
-
-    var gameObj =
-        {
-            id: gameId
-        };
-
-    var playerObj = {
+    // var gameObj =
+    //     {
+    //         id: gameId
+    //     };
+console.log(req.query.playerRole+"------game.js");
+console.log(req.user.id+"-------game.js");
+console.log("game ID : "+gameId);
+var role=req.query.playerRole; 
+var userId=req.user.id;   
+var playerObj = {
         gameId: gameId,
         userId: req.user.id,
         Xposition: 0,
         Yposition: 0,
-        playerRole: 0
+        playerRole: req.query.playerRole
     };
 
-    Game.create(gameObj);
     Player.create(playerObj);
+    
+    db.any('select * from players where "gameId"='+gameId)
+    .then(function(result){
+        pId=id[0]["id"];
+        console.log("Result : "+result);
+    });
+
+    // Player.findOne({ where: {'gameId':gameId} }).then(plyr => {
+    //     var pId=plyr.id;
+    //     console.log("player ID : "+pId);
+    // })
+
+    
+    //console.log("player ID : "+);
+if(role==1){
+    console.log("role is 1");
+    var gameObj =
+    {
+        id: gameId,
+        //copId:
+    };
+}
+if(role==2){
+    console.log("role is 2");
+    var gameObj =
+    {
+        id: gameId
+        //robberId:
+    };
+}
+    Game.create(gameObj);
+    
 
 //    res.json(req.body.playerRole);
 
